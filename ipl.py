12c56cd01560ea6fa1a793f9c572e5ef20de6ec7
gudiya-archi <archijaiswal461@gmail.com>
2025-08-23 22:22:32 +0530

    Add files via upload

--- ipl.py
+++ ipl.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-ipl_matches = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRIaxEzb-Qy5D5usrLZlH98aVh-EmUxyAtFYj35oaIy8U6BVTCp8rBR_M3b3fHnCgz1zroEgpkz-OAu/pub?output=csv"
+ipl_matches = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRj9eesx6RBBnFY1UiP0L2c7skH4n0Kw5_hbnVmwdPi7t7Je2T6aZBoRgMvbplWP4GmP4Bsa50yBQ2J/pub?output=csv"
 matches = pd.read_csv(ipl_matches)
 
 print(matches.head())
